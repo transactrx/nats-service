eb7a6e5cf0b8213471980833b86b3460124f5f79
Fernando Azouth <fazouth@transactrx.com>
2025-02-17 11:31:01 -0500

    Remove timestamp from NATS connection message body
    The default logger being used in the code already provides the date and
    time in the log message prefix by default. Therefore, adding it to the
    log message itself obscures the actual success/error message.

--- pkg/nats-service/nats-service.go
+++ pkg/nats-service/nats-service.go
@@ -92,10 +92,10 @@ func NewLowLevelDebug(basePath, natsQueueName, natsUrl, natsToken, natsKey strin
 	opts = setupConnOptions(opts)
 	nc, err := nats.Connect(natsUrl, opts...)
 	if err != nil {
-		log.Printf("%s Connect failed error: %s", time.Now(), err)
+		log.Printf("ERROR: Failed to connect to NATS server at %s: %s", natsUrl, err)
 		return nil, err
 	}
-	log.Printf("%s Connect CONNECTED to %s SUCCESS ", time.Now(), natsUrl)
+	log.Printf("Connected to NATS server at %s", natsUrl)
 
 	ns := NatService{
 		url:                   natsUrl,
